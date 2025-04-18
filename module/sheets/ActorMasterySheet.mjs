6b3bd977ba066b275fbb008d5f656ae0603786b6
forelius <140267826+Forelius@users.noreply.github.com>
2025-04-18 11:23:22 -0700

    * Actor mastery sheet converted to ItemSheetV2

--- module/sheets/ActorMasterySheet.mjs
+++ module/sheets/ActorMasterySheet.mjs
@@ -1,50 +1,41 @@
+const { HandlebarsApplicationMixin } = foundry.applications.api;
+const { ItemSheetV2 } = foundry.applications.sheets;
 import { EffectManager } from '../sys/EffectManager.mjs';
 /**
  * Sheet class for ActorMasteryItem.
  */
-export class ActorMasterySheet extends ItemSheet {
+export class ActorMasterySheet extends HandlebarsApplicationMixin(ItemSheetV2) {
    /**
     * Get the default options for the ActorMasteryItem sheet.
     */
-   static get defaultOptions() {
-      return foundry.utils.mergeObject(super.defaultOptions, {
-         classes: ['fantastic-depths', 'sheet', 'item'],
-         template: "systems/fantastic-depths/templates/item/ActorMasterySheet.hbs",
-         width:540,
-         height: 240,
+   static DEFAULT_OPTIONS = {
+      window: {
          resizable: true,
-         tabs: [
-            {
-               navSelector: '.sheet-tabs',
-               contentSelector: '.sheet-body',
-               initial: 'attributes',
-            },
-         ],
-      });
+         minimizable: false
+      },
+      classes: ['fantastic-depths', 'sheet', 'item'],
+      position: {
+         width: 540,
+      },
+      form: {
+         submitOnChange: true
+      }
+   };
+
+   static PARTS = {
+      header: {
+         template: "systems/fantastic-depths/templates/item/ActorMasterySheet.hbs"
+      }     
    }
 
    /**
     * Prepare data to be used in the Handlebars template.
     */
-   async getData(options) {
-      const context = await super.getData(options);
-      const itemData = context.data;
-      // Enrich description info for display
-      // Enrichment turns text like `[[/r 1d20]]` into buttons
-      context.enrichedDescription = await TextEditor.enrichHTML(
-         this.item.system.description,
-         {
-            // Whether to show secret blocks in the finished html
-            secrets: this.document.isOwner,
-            // Necessary in v11, can be removed in v12
-            async: true,
-            // Data to fill in for inline rolls
-            rollData: this.item.getRollData(),
-            // Relative UUID resolution
-            relativeTo: this.item,
-         }
-      );
-      context.system = itemData.system;
+   async _prepareContext(options) {
+      const context = {};
+
+      context.item = this.item;
+      context.system = this.item.system;
       context.config = CONFIG.FADE;
       context.isGM = game.user.isGM;
 
@@ -61,9 +52,6 @@ export class ActorMasterySheet extends ItemSheet {
       context.masteryLevels = [...CONFIG.FADE.MasteryLevels.map((key) => {
          return { value: key, text: game.i18n.localize(`FADE.Mastery.levels.${key}`) }
       })].reduce((acc, item) => { acc[item.value] = item.text; return acc; }, {});
-
-      // Prepare active effects for easier access
-      context.effects = EffectManager.prepareActiveEffectCategories(this.item.effects);
 
       return context;
    }
