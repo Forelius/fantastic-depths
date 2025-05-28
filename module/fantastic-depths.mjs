8061ee290084de7d9adfa2a659faf9e15a948776
forelius <140267826+Forelius@users.noreply.github.com>
2025-05-28 07:21:59 -0700

    * Fixed defect preventing compendium rolltables, cards and other from being drag/dropped onto macro hotbar.

--- module/fantastic-depths.mjs
+++ module/fantastic-depths.mjs
@@ -238,7 +238,6 @@ Hooks.once('ready', async function () {
    // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
    Hooks.on('hotbarDrop', (bar, data, slot) => {
       MacroManager.createItemMacro(data, slot);
-      return false;
    });
 
    LightManager.initialize();
