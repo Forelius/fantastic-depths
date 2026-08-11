28fbdfd21d4689aad9823de07afad1d87823d019
forelius <140267826+Forelius@users.noreply.github.com>
2026-08-11 05:21:58 -0700

    Enhance registry so that other modules can easily override system, pre init.

--- src/sys/registry/UserTables.ts
+++ src/sys/registry/UserTables.ts
@@ -10,7 +10,10 @@ export class UserTables {
    constructor() {
       this.#userTables = {};
       this.#loadTables();
-      this.#tryAddDefaultTables();
+   }
+
+   Init() {
+      this.tryAddDefaultTables(this.#userTables);
    }
 
    getTables(): Record<string, UserTableType> {
@@ -168,10 +171,10 @@ export class UserTables {
       UserTablesConfig.displayForm();
    }
 
-   #tryAddDefaultTables() {
+   tryAddDefaultTables(userTables) {
       // Ranged Modifiers
-      if (this.#userTables["ranged-modifiers"] === undefined) {
-         this.#userTables["ranged-modifiers"] = {
+      if (userTables["ranged-modifiers"] === undefined) {
+         userTables["ranged-modifiers"] = {
             id: "ranged-modifiers",
             name: "Ranged Modifiers",
             type: "keyvalue",
@@ -183,8 +186,8 @@ export class UserTables {
          };
       }
       // Ability Score Modifiers - Simple
-      if (this.#userTables["ability-mods-simple"] === undefined) {
-         this.#userTables["ability-mods-simple"] = {
+      if (userTables["ability-mods-simple"] === undefined) {
+         userTables["ability-mods-simple"] = {
             id: "ability-mods-simple",
             name: "Ability Modifiers - Simple",
             type: "jsonarray",
@@ -196,8 +199,8 @@ export class UserTables {
          };
       }
       // Ability Score Modifiers - Heroic
-      if (this.#userTables["ability-mods-heroic"] === undefined) {
-         this.#userTables["ability-mods-heroic"] = {
+      if (userTables["ability-mods-heroic"] === undefined) {
+         userTables["ability-mods-heroic"] = {
             id: "ability-mods-heroic",
             name: "Ability Modifiers - Heroic",
             type: "jsonarray",
@@ -231,8 +234,8 @@ export class UserTables {
             ]
          };
       }
-      if (this.#userTables["difficulty-levels"] === undefined) {
-         this.#userTables["difficulty-levels"] = {
+      if (userTables["difficulty-levels"] === undefined) {
+         userTables["difficulty-levels"] = {
             id: "difficulty-levels",
             name: "Difficulty Levels",
             type: "keyvalue",
@@ -245,8 +248,8 @@ export class UserTables {
             ]
          };
       }
-      if (this.#userTables["tiered-results"] === undefined) {
-         this.#userTables["tiered-results"] = {
+      if (userTables["tiered-results"] === undefined) {
+         userTables["tiered-results"] = {
             id: "tiered-results",
             name: "Tiered Results",
             type: "keyvalue",
@@ -259,8 +262,8 @@ export class UserTables {
             ]
          };
       }
-      if (this.#userTables["ability-mods-original"] === undefined) {
-         this.#userTables["ability-mods-original"] = {
+      if (userTables["ability-mods-original"] === undefined) {
+         userTables["ability-mods-original"] = {
             id: "ability-mods-original",
             name: "Ability Modifiers - Original",
             type: "keyjson",
--- src/sys/registry/fadeRegistry.ts
+++ src/sys/registry/fadeRegistry.ts
@@ -24,75 +24,108 @@ export class fadeRegistry {
    // TODO: These if/else blocks shouldn't be necessary. Registry should know how to handle or move out of registry.
    registerDefaultSystems() {
       // UserTables first, because others may rely on a user table.
-      this.registerSystem("userTables", new UserTables(), UserTables);
-
-      const abilityScoreMods = game.settings.get(game.system.id, "abilityScoreMods");
-      if (abilityScoreMods === "original") {
-         this.registerSystem("abilityScore", new AbilityScoreOriginal(), AbilityScoreOriginal);
-      } else {
-         this.registerSystem("abilityScore", new AbilityScoreRetro(), AbilityScoreRetro);
+      if (this.hasSystem("userTables") === false) {
+         const userTables = new UserTables();
+         userTables.Init();
+         this.registerSystem("userTables", userTables, UserTables);
       }
 
-      this.registerSystem("moraleCheck", new MoraleCheck(), MoraleCheck);
-
-      const abilityCheckSetting = game.settings.get(game.system.id, "abilityCheck");
-      if (abilityCheckSetting === "basic") {
-         this.registerSystem("abilityCheck", new AbilityCheck(), AbilityCheck);
-      } else if (abilityCheckSetting === "tiered") {
-         this.registerSystem("abilityCheck", new TieredAbilityCheck(), TieredAbilityCheck);
+      if (this.hasSystem("abilityScore") === false) {
+         const abilityScoreMods = game.settings.get(game.system.id, "abilityScoreMods");
+         if (abilityScoreMods === "original") {
+            this.registerSystem("abilityScore", new AbilityScoreOriginal(), AbilityScoreOriginal);
+         } else {
+            this.registerSystem("abilityScore", new AbilityScoreRetro(), AbilityScoreRetro);
+         }
       }
-      this.registerSystem("armorSystem", new ClassicArmorSystem(), ClassicArmorSystem);
-      this.registerSystem("damageSystem", new DamageSystem(), DamageSystem);
+
+      if (this.hasSystem("moraleCheck") === false) {
+         this.registerSystem("moraleCheck", new MoraleCheck(), MoraleCheck);
+      }
+
+      if (this.hasSystem("abilityCheck") === false) {
+         const abilityCheckSetting = game.settings.get(game.system.id, "abilityCheck");
+         if (abilityCheckSetting === "basic") {
+            this.registerSystem("abilityCheck", new AbilityCheck(), AbilityCheck);
+         } else if (abilityCheckSetting === "tiered") {
+            this.registerSystem("abilityCheck", new TieredAbilityCheck(), TieredAbilityCheck);
+         }
+      }
+
+      if (this.hasSystem("armorSystem") === false) {
+         this.registerSystem("armorSystem", new ClassicArmorSystem(), ClassicArmorSystem);
+      }
+      if (this.hasSystem("damageSystem") === false) {
+         this.registerSystem("damageSystem", new DamageSystem(), DamageSystem);
+      }
 
       // Load weaponMastery before toHitSystem.
-      const masterySetting = game.settings.get(game.system.id, "weaponMastery");
-      if (masterySetting === "classic") {
-         this.registerSystem("weaponMastery", new WeaponMasteryBase(), WeaponMasteryBase);
-      } else if (masterySetting === "heroic") {
-         this.registerSystem("weaponMastery", new WeaponMasteryHeroic(), WeaponMasteryHeroic);
+      if (this.hasSystem("weaponMastery") === false) {
+         const masterySetting = game.settings.get(game.system.id, "weaponMastery");
+         if (masterySetting === "classic") {
+            this.registerSystem("weaponMastery", new WeaponMasteryBase(), WeaponMasteryBase);
+         } else if (masterySetting === "heroic") {
+            this.registerSystem("weaponMastery", new WeaponMasteryHeroic(), WeaponMasteryHeroic);
+         }
       }
 
-      const toHitSystem = game.settings.get(game.system.id, "toHitSystem");
-      if (toHitSystem === "thac0") {
-         this.registerSystem("toHitSystem", new ToHitTHAC0(), ToHitTHAC0);
-      } else if (toHitSystem === "aac") {
-         this.registerSystem("toHitSystem", new ToHitAAC(), ToHitAAC);
-      } else if (toHitSystem === "classic") {
-         this.registerSystem("toHitSystem", new ToHitClassic(), ToHitClassic);
-      } else if (toHitSystem === "darkdungeons") {
-         this.registerSystem("toHitSystem", new ToHitDarkDungeons(), ToHitDarkDungeons);
-      } else if (toHitSystem === "heroic") {
-         this.registerSystem("toHitSystem", new ToHitHeroic(), ToHitHeroic);
+      if (this.hasSystem("toHitSystem") === false) {
+         const toHitSystem = game.settings.get(game.system.id, "toHitSystem");
+         if (toHitSystem === "thac0") {
+            this.registerSystem("toHitSystem", new ToHitTHAC0(), ToHitTHAC0);
+         } else if (toHitSystem === "aac") {
+            this.registerSystem("toHitSystem", new ToHitAAC(), ToHitAAC);
+         } else if (toHitSystem === "classic") {
+            this.registerSystem("toHitSystem", new ToHitClassic(), ToHitClassic);
+         } else if (toHitSystem === "darkdungeons") {
+            this.registerSystem("toHitSystem", new ToHitDarkDungeons(), ToHitDarkDungeons);
+         } else if (toHitSystem === "heroic") {
+            this.registerSystem("toHitSystem", new ToHitHeroic(), ToHitHeroic);
+         }
       }
 
-      const encSetting = game.settings.get(game.system.id, "encumbrance");
-      if (encSetting === "classic") {
-         this.registerSystem("encumbranceSystem", new ClassicEncumbrance(null), ClassicEncumbrance);
-      } else if (encSetting === "expert") {
-         this.registerSystem("encumbranceSystem", new ExpertEncumbrance(null), ExpertEncumbrance);
-      } else {
-         this.registerSystem("encumbranceSystem", new BasicEncumbrance({ encSetting }), BasicEncumbrance);
+      if (this.hasSystem("encumbranceSystem") === false) {
+         const encSetting = game.settings.get(game.system.id, "encumbrance");
+         if (encSetting === "classic") {
+            this.registerSystem("encumbranceSystem", new ClassicEncumbrance(null), ClassicEncumbrance);
+         } else if (encSetting === "expert") {
+            this.registerSystem("encumbranceSystem", new ExpertEncumbrance(null), ExpertEncumbrance);
+         } else {
+            this.registerSystem("encumbranceSystem", new BasicEncumbrance({ encSetting }), BasicEncumbrance);
+         }
       }
 
-      const initiativeMode = game.settings.get(game.system.id, "initiativeMode");
-      if (initiativeMode === "group") {
-         this.registerSystem("initiativeSystem", new GroupInit(), GroupInit);
-      } else if (initiativeMode === "advancedGroup") {
-         this.registerSystem("initiativeSystem", new AltGroupInit(), AltGroupInit);
-      } else {
-         this.registerSystem("initiativeSystem", new IndivInit(), IndivInit);
+      if (this.hasSystem("initiativeSystem") === false) {
+         const initiativeMode = game.settings.get(game.system.id, "initiativeMode");
+         if (initiativeMode === "group") {
+            this.registerSystem("initiativeSystem", new GroupInit(), GroupInit);
+         } else if (initiativeMode === "advancedGroup") {
+            this.registerSystem("initiativeSystem", new AltGroupInit(), AltGroupInit);
+         } else {
+            this.registerSystem("initiativeSystem", new IndivInit(), IndivInit);
+         }
       }
 
-      this.registerSystem("wrestling", Wrestling, Wrestling);
-      this.registerSystem("shove", Shove, Shove);
-      this.registerSystem("actorMovement", ActorMovement, ActorMovement);
-      this.registerSystem("ancestrySystem", new AncestrySystem(), AncestrySystem);
+      if (this.hasSystem("wrestling") === false) {
+         this.registerSystem("wrestling", Wrestling, Wrestling);
+      }
+      if (this.hasSystem("shove") === false) {
+         this.registerSystem("shove", Shove, Shove);
+      }
+      if (this.hasSystem("actorMovement") === false) {
+         this.registerSystem("actorMovement", ActorMovement, ActorMovement);
+      }
+      if (this.hasSystem("ancestrySystem") === false) {
+         this.registerSystem("ancestrySystem", new AncestrySystem(), AncestrySystem);
+      }
 
-      const classSystem = game.settings.get(game.system.id, "classSystem");
-      if (classSystem === "single") {
-         this.registerSystem("classSystem", new SingleClassSystem(), SingleClassSystem);
-      } else if (classSystem === "advanced") {
-         this.registerSystem("classSystem", new MultiClassSystem(), MultiClassSystem);
+      if (this.hasSystem("classSystem") === false) {
+         const classSystem = game.settings.get(game.system.id, "classSystem");
+         if (classSystem === "single") {
+            this.registerSystem("classSystem", new SingleClassSystem(), SingleClassSystem);
+         } else if (classSystem === "advanced") {
+            this.registerSystem("classSystem", new MultiClassSystem(), MultiClassSystem);
+         }
       }
 
       this.registerSystem("randomCharacter", RandomCharacter, RandomCharacter);
@@ -100,6 +133,10 @@ export class fadeRegistry {
 
    registerSystem(id, instance, type) {
       this.systemDictionary[id] = { id, type, instance };
+   }
+
+   hasSystem(id) {
+      return (this.systemDictionary[id] === null || this.systemDictionary[id] === undefined) == false;
    }
 
    getSystem(id, getAsObject = false) {
